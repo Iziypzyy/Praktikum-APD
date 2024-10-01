4934dcd3e3946679044a1c330d88501d271baa2f
Iziypzyy <zifaalfatih@gmail.com>
2024-10-01 15:48:05 +0800

    posttest 4

--- Kelas A2'24/Post-Test/Post-Test-4/2409106025_AkhmadZifaAlFatih_A2'24_POSTTEST4.py
+++ Kelas A2'24/Post-Test/Post-Test-4/2409106025_AkhmadZifaAlFatih_A2'24_POSTTEST4.py
@@ -1,27 +1,50 @@
-#Menambah fitur login, user menginput username atau password
-#pertama saya menggunakan tipe list data dictionary untuk memanggil kembali.
-
-
-
-
 username = "zifa"
 password = "25" 
 percobaan = 0
 max_percobaan = 3
 
-print("=== Silakan untuk Login terlebih dahulu ===")
-masukkan_username = input("Masukkan username: ")
-masukkan_password = input("Masukkan password: ")
+while percobaan < max_percobaan:
+    print("=== Silakan untuk Login terlebih dahulu ===")
+    masukkan_username = input("Masukkan username: ")
+    masukkan_password = input("Masukkan password: ")
 
-while percobaan > max_percobaan:
-    if masukkan_username == username:
-        print("Login berhasil! Selamat datang.") 
-    if masukkan_password == password :
+    if masukkan_username == username and masukkan_password == password:
         print("Login berhasil! Selamat datang.")
-        break 
+        while True:
+            print("===masukkan data pinjaman===")
+            Nama = input("Masukkan Nama = ")
+            Nim = int(input("Masukkan Nim = "))
+            Jumlah_Pinjaman = int(input("Masukkan Jumlah Pinjaman = "))
+            tahun_cicilan = int(input("Masukkan lama cicilan (1/2/3) = "))
+
+            jumlah_bulan = tahun_cicilan * 12
+            Bunga = {
+                "Bunga1" : 0.7,
+                "Bunga2" : 0.13,
+                "Bunga3" : 0.19
+            }
+
+            if tahun_cicilan == 1:
+                bunga = (Bunga["Bunga1"]/12) * Jumlah_Pinjaman
+                cicilan_bulan = (Jumlah_Pinjaman + bunga) / jumlah_bulan
+            elif tahun_cicilan == 2:
+                bunga = (Bunga["Bunga2"]/12) * Jumlah_Pinjaman
+                cicilan_bulan = (Jumlah_Pinjaman + bunga) / jumlah_bulan
+            elif tahun_cicilan == 3:
+                bunga = (Bunga["Bunga2"]/12) * Jumlah_Pinjaman
+                cicilan_bulan = (Jumlah_Pinjaman + bunga) / jumlah_bulan
+            else:
+                print("Input tidak sesuai dengan persyaratan yang ditentukan!")
+
+            print(f"""{Nama} dengan NIM {Nim} 
+                harus membayar cicilan sebesar Rp.{cicilan_bulan:.0f} 
+                setiap bulan setelah ditambahkan dengan bunga""")
+
+            angka = int(input("Masukkan angka positif jika ingin lanjut (input negatif untuk berhenti): "))
+            if angka < 0:
+                exit()
     else:
         percobaan += 1
         print(f"Username atau Password anda salah!, silakan coba lagi, kesempatan anda tersisa {percobaan - max_percobaan}") 
-
-if percobaan == max_percobaan:
-    print("Anda sudah mencoba sebanyak 3 kali, periksa kembali Username dan Password anda!")
+        if percobaan == max_percobaan:
+            print("Anda sudah mencoba sebanyak 3 kali, periksa kembali Username dan Password anda!")
